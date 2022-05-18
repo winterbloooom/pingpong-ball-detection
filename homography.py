2f19eef8137f215e4bbe626f66e3e3070415481a
YOON-JAEHO <33013780+dkssud8150@users.noreply.github.com>
2022-05-18 13:58:31 +0900

    Update homography.py

--- homography.py
+++ homography.py
@@ -5,6 +5,7 @@
 3. undistort
 4. obj point -> homogeneous 좌표계로 변환
 5. solvePnP, drawFrameAxes로 좌표축 확인, 0,0,0 좌표로 그려져야 맞는것
-6. 
-
+6. findHomography 로 perspective matrix 추출
+7. image point -> homogeneous 좌표계로 변환
+8. homography와 image point를 내적하면 x,y,z 가 추출됨.
 """
